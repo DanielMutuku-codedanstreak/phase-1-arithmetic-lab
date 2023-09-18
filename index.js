413d6e25c7528fc35a3f30d37d7fa5df5fcd4797
Daniel Mutuku <danielmakau507@gmail.com>
2023-09-18 14:48:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+// Define the multiply expression
+function firstCheck(x, y) {
+    x = parseInt(x, 10); // Parse x as an integer
+    y = parseInt(y, 10); 
+    return x * y;
+}
+
+// Define the values of num1 and num2
+let num1 = 2;
+let num2 = 31;
+const multiply = firstCheck(num1, num2);
+//Define the random expression that generates a number greater than 0
+const random = (Math.floor(Math.random() * 10) + 1)
+//Define the mod expression
+function secondCheck(x,y){
+    x = parseInt(x, 10)
+    y = parseInt(y, 10)
+    return num3 % num4
+}
+
+//Define values of num3 and num4
+let num3 = 29
+let num4 = 5
+const mod = secondCheck(num3,num4)
+// Define an expression that returns 20 as the highest number in the set
+const max = Math.max(10,15,20)
